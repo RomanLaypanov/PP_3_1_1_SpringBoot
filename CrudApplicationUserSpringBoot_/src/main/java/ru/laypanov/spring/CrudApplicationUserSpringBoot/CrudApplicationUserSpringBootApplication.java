7bf25931a9d0d461b1ac8b66dcdf25c5c3a03305
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-09-07 21:34:29 +0300

    Первая проверка

---
+++ CrudApplicationUserSpringBoot_/src/main/java/ru/laypanov/spring/CrudApplicationUserSpringBoot/CrudApplicationUserSpringBootApplication.java
@@ -0,0 +1,12 @@
+package ru.laypanov.spring.CrudApplicationUserSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplicationUserSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudApplicationUserSpringBootApplication.class, args);
+    }
+}
---
+++ CrudApplicationUserSpringBoot_/src/main/java/ru/laypanov/spring/CrudApplicationUserSpringBoot/dao/UserDao.java
@@ -0,0 +1,39 @@
+package ru.laypanov.spring.CrudApplicationUserSpringBoot.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.laypanov.spring.CrudApplicationUserSpringBoot.model.User;
+
+
+import java.util.List;
+
+@Component
+@Transactional
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<User> index() {
+        return entityManager.createQuery("from User ", User.class).getResultList();
+    }
+
+    public User show(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    public void save(User user) {
+        entityManager.persist(user);
+    }
+
+    public void update(User updateUser) {
+        entityManager.merge(updateUser);
+    }
+
+    public void delete(int id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+    }
+}
